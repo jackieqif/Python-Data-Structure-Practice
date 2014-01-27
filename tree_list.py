cf0803aa2d08a16df7918d4d1be4fdf6312a7330
jackieqif <jackieqi@gmail.com>
2014-01-27 13:54:33 -0800

    tree using list of list

---
+++ tree_list.py
@@ -0,0 +1,48 @@
+"""Tree data structure using list (of list)."""
+
+
+def makeTree(obj):
+    return [obj, [], []]
+
+
+def getRootVal(tree):
+    return tree[0]
+
+
+def getRightChild(tree):
+    return tree[2]
+
+
+def getLeftChild(tree):
+    return tree[1]
+
+
+def setLeftChild(tree, left):
+    original = tree[1]
+    if len(original) > 0:
+        tree[1] = makeTree(left)
+        tree[1][1] = original
+    else:
+        tree[1] = makeTree(left)
+
+
+def setRightChild(tree, right):
+    original = tree[2]
+    if len(original) > 0:
+        tree[2] = makeTree(right)
+        tree[2][2] = original
+    else:
+        tree[2] = makeTree(right)
+
+
+def buildTree():
+    tree = makeTree('a')
+    setLeftChild(tree, 'b')
+    setRightChild(tree, 'c')
+    setRightChild(getLeftChild(tree), 'd')
+    setLeftChild(getRightChild(tree), 'e')
+    setRightChild(getRightChild(tree), 'f')
+    print tree
+    return tree
+
+ttree = buildTree()
